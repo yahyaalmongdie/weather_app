024da19e820abc121e4db6ae51bfa61ce5ab83f0
Yahya Almongdie <90466657+yahyaalmongdie@users.noreply.github.com>
2024-11-28 18:54:43 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:provider/provider.dart';
-
 import 'package:weather_app/cubits/weather_cubit/weather_cubit.dart';
-
 import 'package:weather_app/models/weather_model.dart';
-
 import 'package:weather_app/pages/home_page.dart';
-
 import 'package:weather_app/providers/weather_provider.dart';
 import 'package:weather_app/services/weather_service.dart';
-
-
 void main() {
 
   runApp(BlocProvider(
