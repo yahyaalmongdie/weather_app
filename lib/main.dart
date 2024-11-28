df866ae82611285907009003f4886b9b766d2c14
Yahya Almongdie <90466657+yahyaalmongdie@users.noreply.github.com>
2024-11-28 18:54:15 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
-
 import 'package:flutter_bloc/flutter_bloc.dart';
-
 import 'package:provider/provider.dart';
 
 import 'package:weather_app/cubits/weather_cubit/weather_cubit.dart';
